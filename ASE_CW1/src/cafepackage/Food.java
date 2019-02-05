8486321c094c60ab3750d788312d8e38475512da
chenning17 <chenning17@users.noreply.github.com>
2019-02-05 12:49:21 +0000

    Item class and subclasses snack, drink, food, discount created
    Creation of item class, snack, drink, food, dicount. Tests pass not including the exceptions as have not been implemented yet.

--- ASE_CW1/src/cafepackage/Food.java
+++ ASE_CW1/src/cafepackage/Food.java
@@ -2,4 +2,14 @@ package cafepackage;
 
 public class Food extends Item {
 
+	public Food(String name, String description, double cost, String id) {
+		super(name, description, cost, id);
+	}
+
+	//TODO implement validation function
+	@Override
+	public boolean validateID() {
+		return false;
+	}
+
 }
