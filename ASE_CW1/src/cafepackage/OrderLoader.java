4a48ccce8eee252c0bb908ba93e448c2e85db52b
kta86 <46680825+kta86@users.noreply.github.com>
2019-02-06 16:27:24 +0000

    Update OrderLoader.java

--- ASE_CW1/src/cafepackage/OrderLoader.java
+++ ASE_CW1/src/cafepackage/OrderLoader.java
@@ -1,5 +1,30 @@
 package cafepackage;
 
+import java.sql.Date;
+
 public class OrderLoader extends FileInput {
 
+	//Basic line processor for orders
+	@Override
+	protected void processLine(String inputLine) {
+		
+
+		OrderCollection orderCollection = new OrderCollection(); 
+		
+		try {
+			String parts [] = inputLine.split(",");
+
+			Date timestamp = Date.valueOf(parts[0]);	
+			int customerID = Integer.parseInt(parts[1]);
+			String orderID = parts[2]; //error- should be Item not String
+			
+
+			Order order = new Order(timestamp,customerID,orderID);
+			orderCollection.add(order);
+			
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}		
 }
