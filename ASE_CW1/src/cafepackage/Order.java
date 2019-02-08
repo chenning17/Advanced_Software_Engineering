8f0ac05210c632d44217cca1179d5c39ef4250f8
Ztuu <stuartpdev@gmail.com>
2019-02-08 14:09:48 +0000

    Populated order class
    Everything is there except struct customer id checking. Only requirement on customer ID is that it is a positive int

--- ASE_CW1/src/cafepackage/Order.java
+++ ASE_CW1/src/cafepackage/Order.java
@@ -4,23 +4,39 @@ import java.util.Date;
 
 public class Order {
 
-	public Order(Date date1, int testCustomerID, Item testItem) {
-		// TODO Auto-generated constructor stub
+	private Date timeStamp;
+	private int customerId;
+	private Item orderItem;
+	
+	public Order(Date timeStamp, int customerId, Item orderItem) {
+		
+		//Check valid parameters have been passed
+		if(timeStamp == null) {
+			throw new IllegalArgumentException("Date for an order can't be null");
+		}
+		if(orderItem == null) {
+			throw new IllegalArgumentException("Order item can't be null");
+		}
+		if(customerId <= 0) {
+			throw new IllegalArgumentException("Customer ID must be a positive int");
+		}
+		
+		//Want to clone date so that if the original object is somehow modified it doesn't change this order
+		this.timeStamp = (Date) timeStamp.clone(); 
+		this.customerId = customerId;
+		this.orderItem = orderItem;
 	}
 
 	public Date getTimestamp() {
-		// TODO Auto-generated method stub
-		return null;
+		return (Date)this.timeStamp.clone(); //Return a clone so date can't be modified
 	}
 
 	public int getCustomerId() {
-		// TODO Auto-generated method stub
-		return -1;
+		return this.customerId;
 	}
 
 	public Item getItem() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.orderItem;
 	}
 
 }
