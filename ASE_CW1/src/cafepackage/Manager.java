f0a04bdd3d6f6e3fc966b9fb2769e0515a3f1ced
Ztuu <stuartpdev@gmail.com>
2019-02-12 08:29:23 +0000

    Report is saved
    A report with customer id, item and date for every order is generated. No formatting

--- ASE_CW1/src/cafepackage/Manager.java
+++ ASE_CW1/src/cafepackage/Manager.java
@@ -1,5 +1,8 @@
 package cafepackage;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Iterator;
 
 public class Manager {
@@ -17,7 +20,7 @@ public class Manager {
 		String menuFile = "Menu.csv";
 		System.out.println("Inputting Items from: " + menuFile);
 		ItemLoader itemLoader = new ItemLoader(menuFile);
-		this. menu = itemLoader.loadItems();
+		this.menu = itemLoader.loadItems();
 		//System.out.println("Printing names");
 		int count = 0;
 		for (Item i : this.menu) {
@@ -32,7 +35,50 @@ public class Manager {
 		OrderLoader orderLoader = new OrderLoader(orderFile, this.menu);
 		this.orders = orderLoader.loadOrders();
 		
+		int orderCount = 0;
+		for (Order o : this.orders) {
+			orderCount++;
+			//System.out.println(i.getName());
+		}
+		System.out.println("---\nOrders loaded: " + orderCount);
 		
+		GenerateReport();
+	}
+	
+	void GenerateReport() {
+		
+		BufferedWriter writer = null;
+		
+		//Code based on answers at: https://stackoverflow.com/questions/2885173/how-do-i-create-a-file-and-write-to-it-in-java
+		try {
+			String filename = "output.txt";
+			File outputFile = new File(filename);
+			writer = new BufferedWriter(new FileWriter(outputFile));
+			writer.write("Customer id\tItem\tDate\t\n");
+			
+			for(Order o : this.orders) {
+				writer.write(o.getCustomerId() + "\t");
+				writer.write(o.getItem().getName() + "\t");
+				writer.write(o.getTimestamp().toString() + "\t");
+				writer.write("\n");
+			}
+			
+			
+			System.out.println("Report saved!");
+		}catch(java.io.IOException ioe) {
+			System.out.println("Failed to save report." + ioe.getMessage());
+		}finally {
+			try {
+				writer.close();
+			}catch(Exception ex) {
+				//Do nothing
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Manager manager = new Manager();
+		manager.run();
 	}
 }
 
