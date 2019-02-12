775f99f313ab3ae073b1e7c17bfc8c71754959c6
Ztuu <stuartpdev@gmail.com>
2019-02-12 08:56:01 +0000

    Report now written to a unique file name each day
    Report name uses that days date to create filename

--- ASE_CW1/src/cafepackage/Manager.java
+++ ASE_CW1/src/cafepackage/Manager.java
@@ -3,6 +3,7 @@ package cafepackage;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.Date;
 import java.util.Iterator;
 
 public class Manager {
@@ -51,7 +52,7 @@ public class Manager {
 		
 		//Code based on answers at: https://stackoverflow.com/questions/2885173/how-do-i-create-a-file-and-write-to-it-in-java
 		try {
-			String filename = "output.txt";
+			String filename = "Report "+ new Date().toString().substring(0, 10) + ".txt";
 			File outputFile = new File(filename);
 			writer = new BufferedWriter(new FileWriter(outputFile));
 			writer.write(String.format("%-15s\t%-25s\t%-30s\t\n", "Customer id", "Item", "Date"));
