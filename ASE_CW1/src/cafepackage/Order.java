8ef7aa958e4c7be364b1dda2bc50202f6bf7c448
Ztuu <stuartpdev@gmail.com>
2019-03-04 09:10:53 +0000

    Refactored a little
    Some readability changes

--- ASE_CW1/src/cafepackage/Order.java
+++ ASE_CW1/src/cafepackage/Order.java
@@ -7,30 +7,29 @@ public class Order {
 
 	private Date timeStamp;
 	private int customerId;
-	private ArrayList<Item> orderItem;
-	private static int currentCustomerID = 0; // tracks customerIDs assigned to orders, in order to automatically assign
-												// IDs to newly created orders
-	// changed orderItem to string (above and below)
+	private ArrayList<Item> orderItems;
+	// tracks customerIDs assigned to orders, in order to automatically assign IDs to newly created orders
+	private static int currentCustomerID = 0;
 
 	// constructor used to read in existing orders from CSV files
-	public Order(Date timeStamp, int customerId, ArrayList<Item> orderItem) {
+	public Order(Date timeStamp, int customerId, ArrayList<Item> orderItems) {
 
 		// Check valid parameters have been passed
 		if (timeStamp == null) {
 			throw new IllegalArgumentException("Date for an order can't be null");
 		}
-		if (orderItem == null) {
+		if (orderItems == null) {
 			throw new IllegalArgumentException("Order item can't be null");
 		}
 		if (customerId <= 0) {
 			throw new IllegalArgumentException("Customer ID must be a positive int");
 		}
 
-		// Want to clone date so that if the original object is somehow modified it
+		// Want to clone objects so that if the original object is somehow modified it
 		// doesn't change this order
 		this.timeStamp = (Date) timeStamp.clone();
 		this.customerId = customerId;
-		this.orderItem = orderItem;
+		this.orderItems = (ArrayList<Item>) orderItems.clone();
 
 		// highest pre-existing customer ID ends up being set to the current ID + 1
 		if (this.customerId >= currentCustomerID) {
@@ -53,9 +52,8 @@ public class Order {
 		return this.customerId;
 	}
 
-	// changed to string
 	public ArrayList<Item> getItems() {
-		return this.orderItem;
+		return this.orderItems;
 	}
 
 }
