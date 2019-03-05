fd6f21e432621cf6452e84f627673e10541bc530
kta86 <46680825+kta86@users.noreply.github.com>
2019-03-05 11:46:44 +0000

    Update LogFile.java
    pair programming (Kate-Lynne)

--- ASE_CW1/src/Part_2/LogFile.java
+++ ASE_CW1/src/Part_2/LogFile.java
@@ -7,29 +7,23 @@ import java.io.IOException;
 import java.util.Date;
 
 public class LogFile {
-	
-	private static LogFile single_Instance = null;
 
-	public String s;
+	private static LogFile single_Instance = null;
 
 	// Private constructor restricted only to this class
 	public LogFile() {
-		s = " has Read Log File and timestamp.";
-		
 		this.generateLogFile();
 	}
 
-
 	// Static method instance of Singleton Class
 	public static LogFile getInstance() {
-		
+
 		// to ensure only one instance is created
 		if (single_Instance == null)
 			single_Instance = new LogFile();
 
 		return single_Instance;
 	}
-
 
 	public void generateLogFile() {
 
@@ -52,6 +46,29 @@ public class LogFile {
 				// Do nothing
 			}
 		}
-			}
-
 	}
+	
+	public void writeToLogFile(String inputString) {
+
+		BufferedWriter writer = null;
+
+		try {
+			String filename = "Log File " + new Date().toString().substring(0, 10) + ".txt";
+			File outputFile = new File(filename);
+			writer = new BufferedWriter(new FileWriter(outputFile, true));
+
+			writer.write(String.format(inputString+"\n"));
+
+			//System.out.println("Log File saved!");
+		} catch (IOException e) {
+			System.out.println("Failed to save log file." + e.getMessage());
+		} finally {
+			try {
+				writer.close();
+			} catch (Exception ex) {
+				// Do nothing
+			}
+		}
+	}
+
+}
