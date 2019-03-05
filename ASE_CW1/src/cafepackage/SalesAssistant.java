5d1518ee268f6a62d6456d2d5c13ba799bf54668
kta86 <46680825+kta86@users.noreply.github.com>
2019-03-05 09:46:27 +0000

    Update SalesAssistant.java

--- ASE_CW1/src/cafepackage/SalesAssistant.java
+++ ASE_CW1/src/cafepackage/SalesAssistant.java
@@ -1,5 +1,26 @@
 package cafepackage;
 
-public class SalesAssistant {
+public class SalesAssistant implements Runnable{
+	
+	//-	Take order, sleep for constant amount of time, send order complete message to log file.
+	
+	private OrderQueue queue;
+	
+	public SalesAssistant(OrderQueue queue) {
+		this.queue = queue;
+	}
+	
+	@Override
+	public void run() {
+		for(int i = 0; i < 8; i++) {
+			try { Thread.sleep(100); }
+			catch (InterruptedException e) {}
+			queue.get();
+			}
+	}
+	
+	public String orderCompleted() {
+		return "This Order has been completed";
+	}
 
 }
