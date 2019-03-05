c11118172880e1353c0e17661042f383e76996b0
Kate-Lynne <46728685+Kate-Lynne@users.noreply.github.com>
2019-03-05 11:27:04 +0000

    Updating LogFile Class
    Pair programming (Katja)

--- ASE_CW1/src/Part_2/LogFile.java
+++ ASE_CW1/src/Part_2/LogFile.java
@@ -1,32 +1,57 @@
 package Part_2;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+
 public class LogFile {
+	
+	private static LogFile single_Instance = null;
 
-	public static void main(String args[]) {
+	public String s;
 
-		// instantiating Singleton class with variable x
-		Singleton x = Singleton.getInstance();
-
-		// instantiating Singleton class with variable y
-		Singleton y = Singleton.getInstance();
-
-		// instantiating Singleton class with variable z
-		Singleton z = Singleton.getInstance();
-
-		// changing variable of instance x
-		x.s = (x.s).toUpperCase();
-
-		System.out.println("String from x is " + x.s);
-		System.out.println("String from y is " + y.s);
-		System.out.println("String from z is " + z.s);
-
-		// changing variable of instance z
-		z.s = (z.s).toLowerCase();
-
-		System.out.println("String from x is " + x.s);
-		System.out.println("String from y is " + y.s);
-		System.out.println("String from z is " + z.s);
-
+	// Private constructor restricted only to this class
+	public LogFile() {
+		s = " has Read Log File and timestamp.";
+		
+		this.generateLogFile();
 	}
 
-}
+
+	// Static method instance of Singleton Class
+	public static LogFile getInstance() {
+		
+		// to ensure only one instance is created
+		if (single_Instance == null)
+			single_Instance = new LogFile();
+
+		return single_Instance;
+	}
+
+
+	public void generateLogFile() {
+
+		BufferedWriter writer = null;
+
+		try {
+			String filename = "Log File " + new Date().toString().substring(0, 10) + ".txt";
+			File outputFile = new File(filename);
+			writer = new BufferedWriter(new FileWriter(outputFile, true));
+
+			writer.write(String.format("Log File created on: %s \n\n", new Date().toString().substring(0, 16)));
+
+			System.out.println("Log File saved!");
+		} catch (IOException e) {
+			System.out.println("Failed to save log file." + e.getMessage());
+		} finally {
+			try {
+				writer.close();
+			} catch (Exception ex) {
+				// Do nothing
+			}
+		}
+			}
+
+	}
--- ASE_CW1/src/cafepackage/Run.java
+++ ASE_CW1/src/cafepackage/Run.java
@@ -1,9 +1,16 @@
 package cafepackage;
 
+import Part_2.LogFile;
+
+
 public class Run {
 	
 	public static void main(String[] args) {
 		Manager manager = new Manager();
 		manager.run();
+	
+		LogFile logFile = new LogFile();
+	//	logFile.generateLogFile();
+	
 	}
 }
