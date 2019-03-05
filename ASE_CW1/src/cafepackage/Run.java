f1b6e46a88ac39e0d31713b4baae5e01c7e8ea0f
Stuart <35631708+Ztuu@users.noreply.github.com>
2019-03-05 11:19:28 +0000

    Reverted change to run

--- ASE_CW1/src/cafepackage/Run.java
+++ ASE_CW1/src/cafepackage/Run.java
@@ -3,9 +3,7 @@ package cafepackage;
 public class Run {
 	
 	public static void main(String[] args) {
-		//Manager manager = new Manager();
-		//manager.run();
-		
-		CafeStateGUI gui = new CafeStateGUI(7);
+		Manager manager = new Manager();
+		manager.run();
 	}
 }
