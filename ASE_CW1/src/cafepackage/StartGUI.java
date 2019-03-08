8346ca7a076acb27cb2d1bbf723f9a3869c36b36
kta86 <46680825+kta86@users.noreply.github.com>
2019-03-08 16:42:24 +0000

    Update StartGUI.java
    Allows both menu and order to choose different files

--- ASE_CW1/src/cafepackage/StartGUI.java
+++ ASE_CW1/src/cafepackage/StartGUI.java
@@ -1,12 +1,8 @@
 package cafepackage;
 
-import java.awt.CardLayout;
-import java.awt.TextField;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.util.Hashtable;
-import javax.swing.JFileChooser;
-
+import javax.swing.event.*;
 import javax.swing.*;
 
 public class StartGUI extends JFrame implements ActionListener {
@@ -16,11 +12,12 @@ public class StartGUI extends JFrame implements ActionListener {
 	JPanel pnlOne = new JPanel();
 	JLabel chooseSimSpeed = new JLabel("Choose Simulation Speed:");
 	JSlider simSpeed = new JSlider(0, 10);
+	JLabel speed = new JLabel(); //Shows which speed is selected
 
 	JPanel pnlTwo = new JPanel();
 	JLabel chooseServerNum = new JLabel("Choose the number of servers:");
 	String[] numServers = new String[] { "1", "2", "3", "4", "5" };
-	JComboBox serverNum = new JComboBox<String>(numServers);
+	JComboBox<String> serverNum = new JComboBox<String>(numServers);
 
 	JPanel pnlThree = new JPanel();
 	JTextArea menuChoice = new JTextArea(1, 30);
@@ -32,10 +29,11 @@ public class StartGUI extends JFrame implements ActionListener {
 	JLabel chooseOrder = new JLabel("Choose Order:");
 	JButton orderButton = new JButton("Select File");
 
+
 	// creates overall frame for GUI
 	public StartGUI() {
 		super("WELCOME!"); // window name
-		setSize(500, 300);
+		setSize(500, 350);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setLocation(450, 400);
 		setVisible(true);
@@ -50,51 +48,59 @@ public class StartGUI extends JFrame implements ActionListener {
 		orderChoice.setLineWrap(true);
 		orderChoice.setWrapStyleWord(true);
 
+		//adding panel 1 - choosing simulation speed
 		pnlOne.add(chooseSimSpeed);
 		pnlOne.add(simSpeed);
+		pnlOne.add(speed);
+		//setting up the slider
 		simSpeed.setMajorTickSpacing(5);
 		simSpeed.setPaintTicks(true);
 		simSpeed.setPaintLabels(true);
-		Hashtable position = new Hashtable();
+		Hashtable<Integer, JLabel> position = new Hashtable<Integer, JLabel>();
 		position.put(0, new JLabel("0"));
 		position.put(5, new JLabel("5"));
 		position.put(10, new JLabel("10"));
 		simSpeed.setLabelTable(position);
+		//display speed beside the component
+		simSpeed.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				speed.setText(Integer.toString(((JSlider)e.getSource()).getValue()));
+			}
+		});
 
+		//adding panel 2 - choose number of servers
 		pnlTwo.add(chooseServerNum);
 		pnlTwo.add(serverNum);
 
+		//adding panel 3 - choose menu
 		pnlThree.add(chooseMenu);
 		pnlThree.add(menuChoice);
 		pnlThree.add(menuButton);
-		//menuButton.addActionListener();
+		menuButton.addActionListener(this);
 
+		//adding panel 4 - choose order list
 		pnlFour.add(chooseOrder);
 		pnlFour.add(orderChoice);
 		pnlFour.add(orderButton);
-	
-
-	menuButton.addActionListener(this);
-	orderButton.addActionListener(this);
-	
+		orderButton.addActionListener(this);
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
 		JFileChooser fileSelect = new JFileChooser();
-		
-		fileSelect.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		
+		fileSelect.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 		fileSelect.setAcceptAllFileFilterUsed(true);
-		
-		int rVal = fileSelect.showOpenDialog(null);
-		if (rVal == JFileChooser.APPROVE_OPTION) {
-			menuChoice.setText(fileSelect.getSelectedFile().toString());
-			orderChoice.setText(fileSelect.getSelectedFile().toString());
-		}
+		int file = fileSelect.showOpenDialog(null);
+		if (file == JFileChooser.APPROVE_OPTION) {
+			if (e.getSource() == menuButton) {
+				menuChoice.setText(fileSelect.getSelectedFile().getName());
+			}
+			if (e.getSource() == orderButton) {
+				orderChoice.setText(fileSelect.getSelectedFile().getName());
+			}}
 	} 
-	public static void main(String[] args) {
-		StartGUI startgui = new StartGUI();
 
+	public static void main(String[] args) {
+		new StartGUI();		
 	}
 
 }
